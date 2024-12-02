ecc2425fcf6f07c2b75082d1fd472348c45c1f10
farouking <souissi.farouk@gmail.com>
2024-12-02 05:58:00 +0100

    nodejs

---
+++ Modern JavaScript/Practice/isodate_formatter/index.js
@@ -0,0 +1,6 @@
+import { formatInput, timeSince, formatInputToDate, formatInputToDate2 } from "./utils/formatDate.js";
+
+console.log(formatInput('2001-04-21T09:40:26+0000'));
+console.log(timeSince('1982-11-14T09:40:26+0000'));
+console.log(formatInputToDate('2017-07-25T20:40:26+0000'));
+console.log(formatInputToDate2('2001-09-11T09:40:26+0000'));
