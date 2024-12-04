03a89b0d97649b18a604deb2cfa4947d7b1ae88c
farouking <souissi.farouk@gmail.com>
2024-12-05 00:26:56 +0100

    First_JSX_React

---
+++ Reactjs/core/First JSX/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', margin: '20px' }}>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
